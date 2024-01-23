6e071d678d7be0f895255bfdbce8cc284e9c263f
karamon17 <gevorik18@gmail.com>
2024-01-23 10:18:43 +0400

    forgot to add tests for friends Service. Decided to work on it later, after creation all modules and interfaces

---
+++ HomeWork6/SocialNetwork/Friends/src/test/java/services/FriendsServiceTest.java
@@ -0,0 +1,47 @@
+package services;
+
+import domain.Profile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FriendsServiceTest {
+
+    @Mock
+    FriendsService service;
+
+    Profile profile1;
+    Profile profile2;
+
+    @BeforeEach
+    void setup(){
+        profile1 = new Profile();
+        profile2 = new Profile();
+    }
+
+    @Test
+    void addFriend() {
+        
+    }
+
+    @Test
+    void getFriends() {
+    }
+
+    @Test
+    void deleteFriend() {
+    }
+
+    @Test
+    void amountFriends() {
+    }
+
+    @Test
+    void isFriends() {
+    }
+}
