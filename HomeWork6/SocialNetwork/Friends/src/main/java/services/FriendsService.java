21a7148d2bc355963b9984e27347f4114a9117c0
karamon17 <gevorik18@gmail.com>
2024-01-23 10:08:53 +0400

    friendsService is ready with dependency of Profiles

---
+++ HomeWork6/SocialNetwork/Friends/src/main/java/services/FriendsService.java
@@ -0,0 +1,13 @@
+package services;
+
+import domain.Profile;
+
+import java.util.Set;
+
+public interface FriendsService {
+    boolean addFriend(Profile profile1, Profile profile2);
+    Set<Profile> getFriends(Profile profile);
+    boolean deleteFriend(Profile profile1, Profile profile2);
+    Integer amountFriends(Profile profile);
+    Boolean isFriends(Profile profile1, Profile profile2);
+}
